417d3470d41bcc6242a3eef66b8580a9cc8566d5
jongjaepark <pjj186@naver.com>
2023-05-03 17:01:02 +0900

    AppLoading

--- App.tsx
+++ App.tsx
@@ -1,9 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect, useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
 import { StyleSheet, Text, View } from 'react-native';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const preload = async () => {
+      try {
+        const fontToLoad = [Ionicons.font];
+        const fontPromises = fontToLoad.map((font) => Font.loadAsync(font));
+        const imagesToLoad = [
+          require('./assets/logo.png'),
+          'https://raw.githubusercontent.com/nomadcoders/instaclone-native/93a5b77e98eefdf5084bfae44653ba67e4ca312c/assets/logo.png',
+        ];
+        const imagePromises = imagesToLoad.map((image) =>
+          Asset.loadAsync(image),
+        );
+        await Promise.all([...fontPromises, ...imagePromises]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    preload();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (!loading) {
+      await SplashScreen.hideAsync();
+    }
+  }, [loading]);
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text>엑스포 파워</Text>
       <StatusBar style="auto" />
     </View>
