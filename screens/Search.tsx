9b4222a076d1ea70067fe8e41d052fe068c725da
jongjaepark <pjj186@naver.com>
2023-06-27 17:16:47 +0900

    Search part Three

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React, { useEffect } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { ActivityIndicator, Text, TextInput, View } from 'react-native';
 import { NavStackParamList } from '../navigators/SharedStackNav';
 import styled from 'styled-components/native';
 import DismissKeyboard from '../components/DismissKeyboard';
@@ -12,12 +12,24 @@ interface ISearchBoxForm {
 }
 
 const SEARCH_PHOTOS = gql`
-  query searchPhotos($keyword: String!) {
-    searchPhotos(keyword: $keyword) {
+  query searchPhotos($keyword: String!, $page: Int!) {
+    searchPhotos(keyword: $keyword, page: $page) {
       id
       file
     }
   }
+`;
+
+const MessageContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MessageText = styled.Text`
+  margin-top: 15px;
+  color: white;
+  font-weight: 600;
 `;
 
 const Input = styled.TextInput``;
@@ -25,9 +37,19 @@ const Input = styled.TextInput``;
 export default function Search({
   navigation,
 }: StackScreenProps<NavStackParamList, 'Search'>) {
-  const { setValue, register, watch } = useForm<ISearchBoxForm>();
+  const { setValue, register, handleSubmit } = useForm<ISearchBoxForm>();
 
-  const [startQueryFn, { loading, data }] = useLazyQuery(SEARCH_PHOTOS);
+  const [startQueryFn, { loading, data, called }] = useLazyQuery(SEARCH_PHOTOS);
+
+  const onValid = ({ keyword }: ISearchBoxForm) => {
+    console.log(keyword);
+    startQueryFn({
+      variables: {
+        keyword,
+        page: 1,
+      },
+    });
+  };
 
   const SearchBox = () => (
     <TextInput
@@ -39,6 +61,7 @@ export default function Search({
       returnKeyType='search'
       autoCorrect={false}
       onChangeText={(text) => setValue('keyword', text)}
+      onSubmitEditing={handleSubmit(onValid)}
     />
   );
 
@@ -46,22 +69,37 @@ export default function Search({
     navigation.setOptions({
       headerTitle: SearchBox,
     });
-    register('keyword');
+    register('keyword', {
+      required: true,
+    });
   }, []);
 
-  console.log(watch('keyword'));
+  console.log(data);
 
   return (
     <DismissKeyboard>
       <View
         style={{
-          backgroundColor: 'black',
           flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
+          backgroundColor: 'black',
         }}
       >
-        <Text style={{ color: 'white' }}>Search</Text>
+        {loading ? (
+          <MessageContainer>
+            <ActivityIndicator size='large' />
+            <MessageText>Searching...</MessageText>
+          </MessageContainer>
+        ) : null}
+        {!called ? (
+          <MessageContainer>
+            <MessageText>Search by keyword</MessageText>
+          </MessageContainer>
+        ) : null}
+        {data?.searchPhotos !== undefined && data?.searchPhotos.length === 0 ? (
+          <MessageContainer>
+            <MessageText>Could not find anything</MessageText>
+          </MessageContainer>
+        ) : null}
       </View>
     </DismissKeyboard>
   );
